d10bbf8afd2b5d1f286e3b29b958bebde9bf8e8d
PROSENJIT\Prosenjit <prosenjitmalda640@gmail.com>
2022-08-22 19:55:29 +0530

    psudo codes more

---
+++ src/main/java/PsudeCode/psudo18.java
@@ -0,0 +1,12 @@
+package PsudeCode;
+
+public class psudo18 {
+    public static void main(String[] args) {
+        int a=0,b=4,c=4;
+        for (c=4;c<=7;c++){
+            a=(a+b)+a;
+            a=(b+b)+b;
+        }
+        System.out.println((a+b));
+    }
+}
