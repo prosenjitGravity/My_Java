68aacc3f6409e36702fdcb6564d2904b7202d2e7
PROSENJIT\Prosenjit <prosenjitmalda640@gmail.com>
2022-08-22 23:28:58 +0530

    psudo codes more

---
+++ src/main/java/Mathematics/Matrix/OneDtoTwoD.java
@@ -0,0 +1,21 @@
+package Mathematics.Matrix;
+
+public class OneDtoTwoD {
+    public static void main(String[] args) {
+        int[] arr=new int[]{1,2,5,6};
+        int l=arr.length/2;
+        int [][] arr1=new int[l][l];
+        for (int i=0;i<arr1.length;i++){
+            for (int j=0;j<arr1.length;j++){
+                arr1[i][j]=arr[j];
+            }
+        }
+        System.out.println("create array;");
+        for (int i=0;i<arr1.length;i++){
+            for (int j=0;j<arr1.length;j++){
+                System.out.print(arr1[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
